d9a26e111b2324105f6cd632a97ed6b8761e36c0
summer2801 <78505968+summer2801@users.noreply.github.com>
2022-10-03 21:15:02 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project-157
+# Welcome To Comics World Made By Nikhil Shaw .
+# Download the Zip And run it to view the output .
---
+++ border.js
@@ -0,0 +1,39 @@
+AFRAME.registerComponent("cursor-listener", {
+    schema: {
+        selectedItemId: { default: "", type: "string "},
+    },
+    init: function (){
+        this.handleMouseEnterEvents();
+        this.handleMouseLeaveEvents();
+    },
+    handleMouseEnterEvents: function(){
+        this.el.addEventListener("mouseenter", ()=> {
+            const id = this.el.getAttribute("id");
+            const posterId =[
+                "Batman",
+                "spiderman",
+                "captain-marvel",
+                "hulk"
+            ];
+            if (postersId.includes(id)){
+                const postersContainer = document.querySelector("#posters-container");
+                postersContainer.setAttribute("cursor-listener",{
+                    selectedItemId:id,
+                });
+                this.el.setAttribute("material", {color: "#005dff"});
+            }
+        });
+    },
+    handleMouseLeaveEvents: function (){
+        this.el.addEventListener("mouseleave", ()=>{
+            const {selectedItemId}= this.data;
+            if (selectedItemId){
+                const el = document.querySelector(`${selectedItemId}`);
+                const id = el.getAttribute("id");
+                if(id == selectedItemId){
+                    el.setAttribute("material", { color: "#fff"});
+                }
+            }
+        });
+    },
+});
